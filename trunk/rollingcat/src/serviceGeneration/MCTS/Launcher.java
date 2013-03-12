94648e8560bb00077e170638473db06d046c3e23
cedric.duffort@gmail.com <cedric.duffort@gmail.com@1c616f7f-31f5-fd84-351f-20db70987355>
2013-03-12 16:12:31 +0000

    Generation d'un graphe

---
+++ trunk/rollingcat/src/serviceGeneration/MCTS/Launcher.java
@@ -0,0 +1,10 @@
+package serviceGeneration.MCTS;
+
+public class Launcher {
+
+	public static void main(String args[])
+	{
+		Graph graph = GraphFactory.makeTentativeGraph();
+		System.out.println(graph);
+	}
+}
