789d249bb1250da44394117fe00c40b9f75aa361
cedric.duffort@gmail.com <cedric.duffort@gmail.com@1c616f7f-31f5-fd84-351f-20db70987355>
2013-03-14 10:28:05 +0000

    Creation entity model

---
+++ trunk/rollingcat/src/serviceGeneration/LevelWriter.java
@@ -0,0 +1,38 @@
+package serviceGeneration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.lirmm.smile.rollingcat.GameConstants;
+import fr.lirmm.smile.rollingcat.utils.EntityModel;
+
+public class LevelWriter {
+
+	
+	public static String write(List<EntityModel> entities) {
+		String str = "";
+		int i =  0 ;
+		for(EntityModel e : entities)
+		{
+			String s = e.getName() + ";" +  e.getX() + ";" + e.getY();
+			if(i < entities.size() -1)
+			{
+				s+="/";
+			}
+			i++;
+			str+=s;
+		}
+		return str;
+	}
+	
+	public static void main(String args[])
+	{
+		ArrayList<EntityModel> list = new ArrayList<EntityModel>();
+		list.add(new SimpleEntity(0.0f, 2.0f,"cat"));
+		list.add(new SimpleEntity(1.0f, 2.0f,"groundblock"));
+		list.add(new SimpleEntity(2.0f, 2.0f,"groundblock"));
+	
+		System.out.println(LevelWriter.write(list));
+	}
+	
+}
