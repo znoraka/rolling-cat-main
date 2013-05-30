f9506ac1c33f19426a4c9fd868c620e88c2b7724
znoraka@gmail.com <znoraka@gmail.com@1c616f7f-31f5-fd84-351f-20db70987355>
2013-05-30 15:32:42 +0000

    intégration d'images supplémentaires

--- rollingcat/src/fr/lirmm/smile/rollingcat/model/game/GroundBlock.java
+++ rollingcat/src/fr/lirmm/smile/rollingcat/model/game/GroundBlock.java
@@ -3,6 +3,7 @@ package fr.lirmm.smile.rollingcat.model.game;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 
 import fr.lirmm.smile.rollingcat.GameConstants;
+import fr.lirmm.smile.rollingcat.RollingCat;
 
 public class GroundBlock extends Entity {
 
@@ -12,7 +13,7 @@ public class GroundBlock extends Entity {
 	 * @param y
 	 */
 	public GroundBlock(float x, float y) {
-		super(x, y, GameConstants.TEXTURE_GROUNDBLOCK);
+		super(x, y, GameConstants.TEXTURE_GROUNDBLOCK+RollingCat.skin);
 		this.setTouchable(Touchable.disabled);
 	}
 	
