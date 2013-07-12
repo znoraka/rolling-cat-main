f51bf36211c625a25ed78747f4147a400983c8a9
znoraka@gmail.com <znoraka@gmail.com@1c616f7f-31f5-fd84-351f-20db70987355>
2013-07-12 10:11:52 +0000

    modification de l'assessment

--- rollingcat/src/fr/lirmm/smile/rollingcat/model/assessment/Triangle.java
+++ rollingcat/src/fr/lirmm/smile/rollingcat/model/assessment/Triangle.java
@@ -16,7 +16,7 @@ public class Triangle {
 	private int percentage;
 	private double max;
 
-	
+
 	/**
 	 * 
 	 * @param angle l'angle du vecteur par rapport à l'horizontale
@@ -32,7 +32,7 @@ public class Triangle {
 		this.percentage = 0;
 		this.max = (angle == 90)?GameConstants.DISPLAY_HEIGHT:(GameConstants.DISPLAY_WIDTH / 2) / Math.cos((double)(angle * 0.0174532925f));
 	}
-	
+
 	/**
 	 * dessine la zone en rouge avec ses limites en noir et son avancement en vert
 	 * @param sr le shaperender utilisé dans le screen
@@ -41,29 +41,29 @@ public class Triangle {
 	public void render(ShapeRenderer sr, AssessmentScreen as, boolean mustChangeColor){
 		if(as.getSelected() == id)
 		{
-			if(mustChangeColor)
+			if(mustChangeColor | as.getWentInSelectedArea())
 				sr.setColor(0, 0.22f, 1f, 1);
 			else
 				sr.setColor(1, 0.22f, 1f, 1);
 		}
-		
+
 		else
 			sr.setColor(Color.GRAY);
-		
+
 		sr.begin(ShapeType.Filled);
 		sr.triangle(
 				GameConstants.DISPLAY_WIDTH / 2, 0,
 				GameConstants.DISPLAY_WIDTH / 2 + direct.cpy().rotate(degree / 2).x, direct.cpy().rotate(degree / 2).y,
 				GameConstants.DISPLAY_WIDTH / 2 + direct.cpy().rotate(-degree / 2).x, direct.cpy().rotate(-degree / 2).y);
-		
+
 		sr.setColor(Color.GREEN);
 		sr.triangle(
 				GameConstants.DISPLAY_WIDTH / 2, 0,
 				GameConstants.DISPLAY_WIDTH / 2 + progression.cpy().rotate(degree / 2).x, progression.cpy().rotate(degree / 2).y,
 				GameConstants.DISPLAY_WIDTH / 2 + progression.cpy().rotate(-degree / 2).x, progression.cpy().rotate(-degree / 2).y);
-		
+
 		sr.end();
-		
+
 		sr.setColor(Color.BLACK);
 		sr.begin(ShapeType.Line);
 		sr.triangle(
@@ -71,32 +71,36 @@ public class Triangle {
 				GameConstants.DISPLAY_WIDTH / 2 + direct.cpy().rotate(degree / 2).x, direct.cpy().rotate(degree / 2).y,
 				GameConstants.DISPLAY_WIDTH / 2 + direct.cpy().rotate(-degree / 2).x, direct.cpy().rotate(-degree / 2).y);
 		sr.end();
-		
+
 	}
-	
+
 	/**
 	 * met à jour la progression du patient sur une zone
 	 * @param x la position du curseur en x
 	 * @param y la position du curseur en y
 	 */
 	public void setProgression(float x, float y, AssessmentScreen as){
-		Vector2 a = new Vector2(x - GameConstants.DISPLAY_WIDTH / 2, y);
-		
-		if(a.angle() <= direct.cpy().rotate(degree / 2).angle() & (a.angle() >= direct.cpy().rotate(-degree / 2).angle() | direct.angle() < degree)){
 
-			if(a.len() > progression.len() && as.getSelected() == id){
-				progression.set(a);
-				progression.setAngle(direct.angle());
+		if(!as.canStart())
+		{
+			Vector2 a = new Vector2(x - GameConstants.DISPLAY_WIDTH / 2, y);
+			if(a.angle() <= direct.cpy().rotate(degree / 2).angle() & (a.angle() >= direct.cpy().rotate(-degree / 2).angle() | direct.angle() < degree)){
+
+				if(a.len() > progression.len() && as.getSelected() == id){
+					as.setWentInSelectedArea();
+					progression.set(a);
+					progression.setAngle(direct.angle());
+					percentage = Math.abs((int) ((Math.abs(progression.len() / max) * 100 < 100f)?(Math.abs(progression.len()) / max) * 100:100));
+				}
+
 			}
-			percentage = (int) ((Math.abs(progression.len() / max) * 100 < 100f)?(Math.abs(progression.len()) / max) * 100:100);
-
 		}
 	}
 
 	public int getAngle() {
 		return (int) (this.degree * id + this.degree / 2);
 	}
-	
+
 	public int getID() {
 		return this.id;
 	}
